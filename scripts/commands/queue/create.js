fd1c1e7dce3bd57a31b349b0d3a8d15ac491d61c
David <tinuva@ipv6.za.org>
2022-04-26 10:31:36 +0200

    uncomment log

--- scripts/commands/queue/create.js
+++ scripts/commands/queue/create.js
@@ -51,7 +51,7 @@ async function createQueue() {
         //logger.info(`Item ${item.xmltv_id}`)
         if (!item.site || !item.xmltv_id) continue
         const channel = api.channels.find({ id: item.xmltv_id })
-        //logger.info(`Channel ${channel}`)
+        logger.info(`Channel ${channel}`)
         if (!channel) continue
         for (const d of dates) {
           const dString = d.toJSON()
